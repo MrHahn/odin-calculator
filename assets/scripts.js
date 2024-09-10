d7f3756f93f057c33b05c830b53c2ec17c245e13
Christopher Hahn <chahn970@gmail.com>
2024-09-09 19:41:59 -0600

    add datasets to buttons, start calculations logic

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,6 +4,9 @@ let secondNum;
 
 let operator; 
 
+const numberButtons = document.querySelectorAll('.calculator-numbers button');
+let display = document.querySelector('.calculator-screen span');
+
 function add(a, b){
     return a + b;
 }
@@ -35,3 +38,25 @@ function operate(num1, num2, operator){
             return divide(num1, num2);
     }
 }
+
+function updateDisplay(data){
+    display.textContent += data;
+}
+
+function gatherData(){
+    // on every number button click, fire event listener and append (+=) the numbers to eachother in the display
+    updateDisplay(parseInt(this.dataset.num));
+
+
+    // when an operator button is clicked, set the value in the display to the first variable and set the operation
+    // to the operator variable
+
+    // on same event listener for number buttons, check if first num variable is filled, if so, clear the display first
+    // then add the new number.
+
+    // when equals is pressed, this will pass the variables into the operate function and display the returned value to the 
+    // display
+}
+
+
+numberButtons.forEach(button => button.addEventListener('click', gatherData));
